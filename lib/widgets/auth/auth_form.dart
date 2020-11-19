162b23d9f11ccb99ee0e3728158a341cde606d20
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-19 08:47:21 +0100

    auth screen scaffold

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -10,36 +10,45 @@ class _AuthFormState extends State<AuthForm> {
   Widget build(BuildContext context) {
     return Center(
         child: Card(
-      margin: EdgeInsets.all(20),
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Form(child: Column(
-            mainAxisSize: MainAxisSize.min, //children to take min size the need
-            children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'Email Address',
+            margin: EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  child: Column(
+                    mainAxisSize:
+                        MainAxisSize.min, //children to take min size the need
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      TextFormField(
+                        obscureText: true, //hide text for password
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      RaisedButton(
+                        child: Text('Login'),
+                        onPressed: () {},
+                      ),
+                      FlatButton(
+                          onPressed: () {}, child: Text('Create new account'))
+                    ],
+                  ),
+                ),
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            TextFormField(
-              obscureText: true, //hide text for password
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 12,),
-            RaisedButton(child: Text('Login'),onPressed: (){},),
-            FlatButton(onPressed: (){}, child: Text('Create new account'))
-          ],),
-        ),
-      ),
-    ));
+            )));
   }
 }
