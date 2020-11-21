918c78e5322979382d296c95fb9aa44330d5edba
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-11-21 12:39:51 +0100

    switch screen on authentication and logout feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:chatroom/screens/auth_screen.dart';
-import 'package:chatroom/screens/chat_screen%20copy%202.dart';
+import 'package:chatroom/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
