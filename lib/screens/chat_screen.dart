7469464bbf1c793173fd4370c517c73c1ac79b8d
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-12-11 16:18:00 +0100

    notifications state

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,39 @@
 import 'package:chatroom/widgets/chat/messages.dart';
 import 'package:chatroom/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions(); //needed for ios
+    fbm.configure(
+        //app running on foreground
+        onMessage: (msg) {
+      print(msg);
+      return;
+    },
+        //app terminated or not running
+        onLaunch: (msg) {
+      print(msg);
+      return;
+    },
+        //app running in background
+        onResume: (msg) {
+      print(msg);
+      return;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
